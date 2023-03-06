4ff605b74908523ad5e09ed0efa5184466dae2b9
Dobrin <dobrindonev@gmail.com>
2023-03-06 17:11:52 +0200

    Week3: Extends todo, css

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Welcome to Webtech 2023!
 This repository contains Group A's code for the 2023 Webtech course. It's updated on a weekly basis.
+
+
+# Contacts:
+email: dobrindonev@gmail.com
+email2: dobrindd@
---
+++ week3/browser/flex-layout/index.js
@@ -0,0 +1,46 @@
+const input = document.querySelector('input');
+input.addEventListener('keyup', onKeyUp);
+
+function hasInput() {
+    const button = document.querySelector('button');
+    if (!button) return;
+
+    if (input.value === '') {
+        button.setAttribute('disabled', 'true');
+    } else if (button.hasAttribute('disabled')) {
+        button.removeAttribute('disabled');
+    }
+}
+
+function onKeyUp(e) {
+    hasInput();
+    if (e.code === 'Enter') {
+        addToDoItem();
+    }
+}
+
+function addToDoItem() {
+    const ul = document.querySelector('ul');
+    const myInputString = input.value;
+    
+    if (myInputString === '') {
+        return;
+    }
+    
+    const li = document.createElement('li');
+    li.onclick = function (e) {
+        e.currentTarget.remove();
+        hasInput();
+    }
+    li.textContent = myInputString.trim();
+    
+    ul.appendChild(li);
+    
+    input.value = '';
+}
+
+document.onreadystatechange = () => {
+    if (document.readyState === "complete") {
+        hasInput();
+    }   
+};
