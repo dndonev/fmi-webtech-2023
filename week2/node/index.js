87dd9e6a1000c37e0235d06ba37ca5ed96fe34d1
Dobrin <dobrindonev@gmail.com>
2023-03-06 13:53:30 +0200

    Week2: DOM elements, manipulation

---
+++ week2/node/index.js
@@ -0,0 +1,22 @@
+var a;
+// var myNumber = 5;
+// myNumber = 'Dobrin';
+
+// myNumber = 5;
+
+// if (typeof myNumber === 'string') {
+//     myNumber.includes('o');
+// }
+
+// console.log('COOL!')
+// // console.log(myNumber());
+
+// var myString = '5';
+// var myNumber = 5;
+
+// console.log('DOUBLE ', myString == myNumber); // true
+// console.log('TRIPLE ', myString === myNumber); // false
+
+// var a = 'WTF';
+a = 'WTF';
+console.log(a);
